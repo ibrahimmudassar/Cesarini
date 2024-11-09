8adca16ff93e236a8ea3ba135445a6cb54db5a65
srinandan-gondi <115195149+srinandan-gondi@users.noreply.github.com>
2024-11-09 16:06:41 -0500

    made some changes

---
+++ cesarini-template/src/config/url.ts
@@ -0,0 +1,1 @@
+export const url = "http://10.25.6.182:5001/";
--- cesarini-template/src/pages/index.tsx
+++ cesarini-template/src/pages/index.tsx
@@ -7,22 +7,78 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { url } from "@/config/url";
+import { Spinner } from "@nextui-org/spinner";
 
 export default function IndexPage() {
+  
+  const [historicAvg, updateHistoricAvg] = useState("loading");
+  
+  useEffect(() => {
+    
+    axios.get("http://ip-api.com/json/").then((response) => {
+      axios.post(url, {latitude: response.data.lat, longitude: response.data.lon}, {headers: {
+        'Content-Type': 'application/json'
+      }}).then((response) => {
+        updateHistoricAvg(response.data.temp_today);
+      })
+    })
+    
+  },[])
+  
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
+      <div className="inline-block max-w-lg text-center justify-center">
+          
+      <div className={subtitle({ class: "mt-4" })}>
+            The most accurate weather you could ask for.
+          
+      </div>
+          
+          <span className={title()}><span className="no-wrap">Saturday,  Nov 9</span> </span>
+          <br />
+          <br />
+          
+          <span className={title()}>11:45 AM</span>
+          <br />
+          <br />
+          
+          <span className={title({ color: "violet" })}>Feels Like:&nbsp;</span>
+          <br />
+          <br />
+          {historicAvg !== "loading" ? (<span className={title()}>
+              
+              {/* {typeof historicAvg === "string" ? (
+                <p>{historicAvg}°F</p> ) : (
+                  
+                  <p>loading...</p>)} */}
+              
+              {historicAvg}°F
+          </span>) : (<Spinner color="secondary"/>)}
+          
+          <br />
+          <br />
+          
+          <span className={title()}>
+          Clear
+          </span>
+          
+      </div>
+        
+        {/* <div className="inline-block max-w-lg text-center justify-center">
+          <span className={title({color: "cyan"})}>Make&nbsp;</span>
           <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
           <br />
           <span className={title()}>
             websites regardless of your design experience.
           </span>
           <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
+          The most accurate weather you could ask for.
           </div>
-        </div>
+        </div> */}
 
         <div className="flex gap-3">
           <Link
